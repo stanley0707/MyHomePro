73183fba3804a3db115a21d6f39388559d12cf01
Stannis Shilow <shilowstanislaw@gmail.com>
2018-08-19 03:48:54 +0300

    refactoring#2

--- agency/settings.py
+++ agency/settings.py
@@ -34,7 +34,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'shilowstanislaw@gmail.com'
-EMAIL_HOST_PASSWORD = '1234567890QPqwertyuiop'
+EMAIL_HOST_PASSWORD = '*********************'
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
